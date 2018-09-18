b40841fd0dde9d51cfc46f1f4a89e18ab7c9be61
Samuel Nixon <sam.nixon@hotmail.com.au>
2018-09-18 08:22:11 +0000

    working on blog routes and sytling - create first four basic routes

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 "use strict";
 
 //Base Packages
-var     express     = require("express"),
-        app         = express(),
-        bodyParser  = require("body-parser");
-const   sgMail      = require('@sendgrid/mail');
+var     express         = require("express"),
+        app             = express(),
+        mongoose        = require("mongoose"),
+        bodyParser      = require("body-parser"),
+        expressSanitizer= require("express-sanitizer"),
+        Blog            = require("./models/blog");
+const   sgMail          = require('@sendgrid/mail');
+
+// var     GoogleMapAPIKey = require("./googleapi.env");
+//This connects but does not load
 
 //base declaration
 var receivingEmailAdress = "sam@whaley.money";
 
-
+mongoose.connect("mongodb://localhost/ground_search_local");
 app.use(bodyParser.urlencoded({extended: true}));    //Connfigures the usage of app to automatically use Body-Parser as its middleware
 app.use(express.static(__dirname +"/public"));     //Automatically load assets in public folder for use, however remember this needs to be linked in our sheets (usually in our header file)r)
-app.set("view engine","ejs");  
+// app.use(express.static(__dirname +"/scripts")); 
+// console.log(__dirname +"/scripts");
+app.set("view engine","ejs"); 
+app.use(expressSanitizer());
+
 
 //Own Google Cloud Account, potentially replace in the future
-var GoogleMapAPIKey = process.env.APIKEYMAP; //TODO, setup enviroment variable server side. 
-console.log(process.env.APIKEYMAP); //check that enviroment variable is correctly storing APIKEYMAP
+var GoogleMapAPIKey = process.env.APIKEYMAP; // Enviroment Variable for deployment version setup and working
+console.log(GoogleMapAPIKey); //Having trouble storing this locally.
 
 //Own SendGrid Account, 100 free per day on free plan + good analytics
-var SendGridAPI = (process.env.SENDGRID_API_KEY); //TODO, setupEnviromentVatiable
-console.log(SendGridAPI); //check that enviroment variable is correclty setup
+var SendGridAPI = (process.env.SENDGRID_API_KEY); //Enviroment Variable for deployment version setup and working
 sgMail.setApiKey(SendGridAPI);
+console.log(SendGridAPI); //Having trouble storing this locally
+
+
 
 
 app.get("/", function(req, res){
@@ -44,10 +56,47 @@ app.get("/services", function(req, res){
     //does not exist in groundSearch Australia
 });   
 
+//BLOGS//
+
+//Index Route 
 app.get("/blog", function(req, res){
-    res.render("blog");
+    Blog.find({}, function(error, blogs){
+        if (error) {
+            console.log('error');
+        } else {
+            res.render("blog", {blogs:blogs});
+        }
+    });
     //what new in ground search, maybe use the schema I had previously build
 });  
+
+//New Route
+app.get("/blog/new", function (req, res){
+    res.render("new");
+});
+
+//Create Routes - I need to put protection middleware in here
+app.post("/blog", function(req, res){
+    req.sanitize(req.body.blog.body);
+    Blog.create(req.body.blog, function(error, newBlogPost){
+        if (error){
+            res.render("new");
+        } else {
+            res.redirect("/blog");
+        }
+    });
+});
+
+//Show route
+app.get("/blog/:id", function(req, res){
+    Blog.findById(req.params.id, function(error, foundBlog){
+        if(error){
+            res.redirect("/blog");
+        } else {
+            res.render("show",{blog: foundBlog});
+        }
+    });
+});
 
 app.get("/contact", function(req, res){
     res.render("contact", {GoogleMapAPIKey: GoogleMapAPIKey});
@@ -58,7 +107,7 @@ app.post("/contact", function(req, res){
       to: receivingEmailAdress,
       from: req.body.email,
       subject: 'Website Contact Email',
-      html: req.body.msg + "<br>" + "PhoneNumber: " + req.body.phone + "Website: " + "<br>" + req.body.website + "<br><br>" + "From: " + req.body.name,
+      html: req.body.msg + "<br><br>" + "PhoneNumber: " + req.body.phone + "<br>" + "Website: " + req.body.website + "<br><br>" + "From: " + req.body.name,
     };
     sgMail.send(msg);
     res.redirect("home");
